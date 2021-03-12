4471fb20443a25b6c5e503e16e6a0f392e2a25b7
Aigars Gedroics <gedrox@fb.com>
2021-03-12 23:14:15 +0000

    2

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 var lat, lon;
+var test = true;
 
 function step(i, setHistory) {
     if (typeof(setHistory) == 'undefined') {
@@ -12,6 +13,36 @@ function step(i, setHistory) {
     }
 }
 
+function getRandomPosition(lat, lon, distance) {
+    var R = 6378.1;
+    var brng = 2 * Math.PI * Math.random();
+    var d = distance * (1 + (0.2 * Math.random() - 0.1));
+    console.error(d);
+    var lat1 = lat / 180. * Math.PI;
+    var lon1 = lon / 180. * Math.PI;
+
+    var lat2 = Math.asin( Math.sin(lat1)*Math.cos(d/R) +
+         Math.cos(lat1)*Math.sin(d/R)*Math.cos(brng))
+
+    var lon2 = lon1 + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(lat1),
+                 Math.cos(d/R)-Math.sin(lat1)*Math.sin(lat2))
+
+    var lat2 = lat2 / Math.PI * 180.;
+    var lon2 = lon2 / Math.PI * 180.;
+    
+    return [lat2, lon2];
+}
+
+function getPosition(callback) {
+    if (test) {
+        callback(20.0, 21.2);
+    }else {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            callback(position.coords.latitude, position.coords.longitude);
+        });
+    }
+}
+
 $(function(){
 
     step(1);
@@ -19,9 +50,12 @@ $(function(){
         step(2);
     });
     $('#step-2 button').click(function() {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            $('.position').text("Latitude: " + position.coords.latitude +
-                                  "<br>Longitude: " + position.coords.longitude);
+        getPosition(function(lat, lon) {
+            $('.position').html("Latitude: " + lat +
+              "<br>Longitude: " + lon);
+            var target = getRandomPosition(lat, lon, $('#distance').val())
+              $('.target').html("Latitude: " + target[0] +
+                            "<br>Longitude: " + target[1]);
         });
         step(3);
     });
